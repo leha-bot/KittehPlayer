e15acd09494e3124b06c0eef7f22c1dea9bf3e6d
NamedKitten <kitteh@namedkitten.pw>
2018-10-14 19:17:16 +0100

    Try only building mpv.

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/mpv-player/mpv-build
-cd mpv-build
+#git clone --depth 1 https://github.com/mpv-player/mpv-build
+#cd mpv-build
+export PATH="/usr/lib/ccache:$PATH"
+git clone --depth 1 https://github.com/mpv-player/mpv
+cd mpv
 
 echo --enable-libmpv-shared --prefix=/usr  >> mpv_options
 echo --disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-vapoursynth --disable-libsmbclient >> mpv_options
 
-./rebuild -j`nproc`
-sudo ./install
+
+./bootstrap.py
+./waf configure `cat mpv_options`
+./waf -j`nproc`
+sudo ./waf install
+
 cd ..
