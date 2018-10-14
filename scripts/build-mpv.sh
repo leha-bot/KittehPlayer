5e55fb1d19360f751783af6d3992d1ededf0d8fe
NamedKitten <kitteh@namedkitten.pw>
2018-10-14 13:29:02 +0100

    Revert to trusty and try to build MPV from source.

---
+++ scripts/build-mpv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone --depth 1 https://github.com/mpv-player/mpv-build
+cd mpv-build
+
+echo --enable-libmpv-shared > mpv_options
+echo --disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-vapoursynth --disable-libsmbclient > mpv_options
+
+./rebuild -j`nproc`
+sudo ./install
+cd ..
