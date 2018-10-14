64a8ea09426060572e3606358a0dce5ef1a6d9a2
NamedKitten <kitteh@namedkitten.pw>
2018-10-14 23:21:51 +0100

    Add AV1 codec.

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -8,9 +8,18 @@ CFLAGS="-fPIC -Os"
 rm -rf mpv-build && git clone --depth 1 https://github.com/mpv-player/mpv-build mpv-build
 cd mpv-build
 
+git clone --depth 1 https://aomedia.googlesource.com/aom
+mkdir build && cd build
+cmake ../aom -G "Unix Makefiles"
+make -j`nproc`
+sudo make install
+cd ..
+
 rm -rf ffmpeg && git clone --depth 1 https://github.com/FFmpeg/FFmpeg.git ffmpeg
 rm -rf mpv && git clone --depth 1 https://github.com/mpv-player/mpv.git mpv 
 rm -rf libass && git clone --depth 1 https://github.com/libass/libass.git libass
+
+echo "--enable-libaom" > ffmpeg_options
 
 echo "--enable-libmpv-shared --prefix=/usr" > mpv_options
 echo "--disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-vapoursynth --disable-libsmbclient" >> mpv_options
