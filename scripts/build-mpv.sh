dc08a1a4cbf76db9f2ae43de1ff42866fa5bb426
NamedKitten <kitteh@namedkitten.pw>
2018-10-14 14:11:12 +0100

    Add ccache and make MPV install to the right location.

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -2,7 +2,7 @@
 git clone --depth 1 https://github.com/mpv-player/mpv-build
 cd mpv-build
 
-echo --enable-libmpv-shared > mpv_options
+echo --enable-libmpv-shared --prefix=/usr > mpv_options
 echo --disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-vapoursynth --disable-libsmbclient > mpv_options
 
 ./rebuild -j`nproc`
