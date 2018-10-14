f76945b3928bc3d66c532c20533f93bb8ffca599
NamedKitten <kitteh@namedkitten.pw>
2018-10-14 21:26:48 +0100

    FINALLY.

--- scripts/makeappimage.sh
+++ scripts/makeappimage.sh
@@ -4,16 +4,15 @@ export PATH="/usr/lib/ccache:/usr/lib/ccache/bin:$PATH"
 
 bash scripts/build-mpv.sh
 
-export QML_SOURCES_PATH=src/qml
+export QML_SOURCES_PATHS=src/qml
 
 qmake CONFIG+=release PREFIX=/usr
 make -j$(nproc)
 make INSTALL_ROOT=appdir -j$(nproc) install ; find appdir/
 #wget "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-wget "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
-wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+wget -nc "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
+wget -nc https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
 chmod +x linux*
-unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
 #mkdir -p appdir/usr/plugins/imageformats
 #cp /opt/qt*/plugins/imageformats/libqsvg.so appdir/usr/plugins/imageformats/
