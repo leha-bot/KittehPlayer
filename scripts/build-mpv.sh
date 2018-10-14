1cdfb2b5474c35eeb7f70a2c8f5a3a2d4417df6c
NamedKitten <kitteh@namedkitten.pw>
2018-10-14 21:19:13 +0100

    rebuild -> build

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -13,6 +13,6 @@ git clone --depth 1 https://github.com/libass/libass.git libass || true
 echo "--enable-libmpv-shared --prefix=/usr" > mpv_options
 echo "--disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-vapoursynth --disable-libsmbclient" >> mpv_options
 
-./rebuild -j`nproc`
+./build -j`nproc`
 sudo ./install
 cd ..
