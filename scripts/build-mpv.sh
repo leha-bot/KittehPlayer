9fd8c88f19152fd9101c8a69f84ec5f3aa499b1e
NamedKitten <kitteh@namedkitten.pw>
2018-10-15 07:35:58 +0100

    Remove AOM for now.

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -6,22 +6,22 @@ CFLAGS="-fPIC -Os"
 git clone --depth 1 https://github.com/mpv-player/mpv-build mpv-build || true
 cd mpv-build
 
-git clone --depth 1 http://aomedia.googlesource.com/aom aom || true
-mkdir aom-build || true
-cd aom-build
-  cmake -G Ninja ../aom \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DBUILD_SHARED_LIBS=1 \
-    -DENABLE_TESTS=0 -G "Unix Makefiles"
-  cmake --build .
-  sudo cmake --build . --target install
-cd ..
+#git clone --depth 1 http://aomedia.googlesource.com/aom aom || true
+#mkdir aom-build || true
+#cd aom-build
+#  cmake ../aom \
+#    -DCMAKE_INSTALL_PREFIX=/usr \
+#    -DBUILD_SHARED_LIBS=1 \
+#    -DENABLE_TESTS=0 -G "Unix Makefiles"
+#make -j`nproc`
+#sudo make install
+#cd ..
 
 git clone --depth 1 https://github.com/FFmpeg/FFmpeg.git ffmpeg || true
 git clone --depth 1 https://github.com/mpv-player/mpv.git mpv || true
 git clone --depth 1 https://github.com/libass/libass.git libass || true
 
-echo "--enable-libaom" > ffmpeg_options
+#echo "--enable-libaom" > ffmpeg_options
 
 echo "--enable-libmpv-shared --prefix=/usr" > mpv_options
 echo "--disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-vapoursynth --disable-libsmbclient" >> mpv_options
