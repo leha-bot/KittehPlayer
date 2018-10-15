ec44af045e1da3172b3e61dfc0c1a8f8f8d43656
NamedKitten <kitteh@namedkitten.pw>
2018-10-15 02:36:55 +0100

    Boop!

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -6,12 +6,15 @@ CFLAGS="-fPIC -Os"
 git clone --depth 1 https://github.com/mpv-player/mpv-build mpv-build || true
 cd mpv-build
 
-git clone --depth 1 https://github.com/mozilla/aom aom || true
+git clone --depth 1 http://aomedia.googlesource.com/aom aom || true
 mkdir aom-build || true
 cd aom-build
-../aom/configure --enable-pic --enable-av1 --enable-shared --prefix=/usr --disable-sse --disable-runtime-cpu-detect --disable-optimizations --enable-small --disable-mmx --disable-examples --disable-docs --disable-tools 
-make -j`nproc`
-sudo make install
+  cmake -G Ninja ../aom \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DBUILD_SHARED_LIBS=1 \
+    -DENABLE_TESTS=0
+  cmake --build .
+  sudo cmake --build . --target install
 cd ..
 
 git clone --depth 1 https://github.com/FFmpeg/FFmpeg.git ffmpeg || true
