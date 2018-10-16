e3ac07e3b638c7a774ae6dc38ff30453ba603335
NamedKitten <kitteh@namedkitten.pw>
2018-10-16 08:17:37 +0100

    Remove ffmpeg configure cflags.

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -29,7 +29,6 @@ git clone --depth 1 https://github.com/mpv-player/mpv.git mpv || true
 git clone --depth 1 https://github.com/libass/libass.git libass || true
 
 #echo "--enable-libaom" > ffmpeg_options
-echo "--extra-cflags=\'$CFLAGS\'" > ffmpeg_options
 echo "--enable-libmpv-static --prefix=/usr" > mpv_options
 echo "--disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-vapoursynth --disable-libsmbclient" >> mpv_options
 
