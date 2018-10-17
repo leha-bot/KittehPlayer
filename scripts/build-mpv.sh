9f68837a7dfbf002b474d81ac4e7a6e6ed5b0657
NamedKitten <kitteh@namedkitten.pw>
2018-10-17 08:35:17 +0100

    Revert cplayer disable.

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -30,10 +30,10 @@ git clone --depth 1 https://github.com/libass/libass.git libass
 
 #echo "--enable-libaom" > ffmpeg_options
 echo "--disable-programs --disable-runtime-cpudetect --enable-small" > ffmpeg_options
-echo "--enable-libmpv-shared --prefix=/usr --disable-cplayer" > mpv_options
+echo "--enable-libmpv-shared --prefix=/usr" > mpv_options
 echo "--disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-vapoursynth --disable-libsmbclient" >> mpv_options
 
-./rebuild -j`nproc` 2>&1 > tee build.log
+./rebuild -j`nproc` 2>&1 > build.log
 sudo ./install
 ccache -s
 curl --upload-file build.log https://transfer.sh/KittehPlayer-build.log
