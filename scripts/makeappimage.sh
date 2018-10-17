a8fb344185e695623a7046a8a7d417f6abba9be1
NamedKitten <kitteh@namedkitten.pw>
2018-10-17 08:04:21 +0100

    Add youtube-dl to appimage.

--- scripts/makeappimage.sh
+++ scripts/makeappimage.sh
@@ -13,7 +13,8 @@ make -j$(nproc)
 make INSTALL_ROOT=appdir -j$(nproc) install ; find appdir/
 #wget "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 wget -nc "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
-wget -nc https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+wget -nc "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage"
+wget -nc "https://raw.githubusercontent.com/TheAssassin/linuxdeploy-plugin-conda/master/linuxdeploy-plugin-conda.sh"
 chmod +x linux*
 mkdir -p appdir/usr/lib
 cp /usr/lib/x86_64-linux-gnu/libjack.so.0 appdir/usr/lib
@@ -22,4 +23,6 @@ export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for namin
 #cp /opt/qt*/plugins/imageformats/libqsvg.so appdir/usr/plugins/imageformats/
 #./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -qmldir=./src/qml/ -bundle-non-qt-libs
 #./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -qmldir=./src/qml/ -appimage
-./linuxdeploy-x86_64.AppImage --appdir appdir --plugin qt --output appimage
+export PIP_REQUIREMENTS=youtube-dl
+ln -s ../conda/bin/youtube-dl appdir/usr/bin/youtube-dl
+./linuxdeploy-x86_64.AppImage --appdir appdir --plugin qt --plugin conda --output appimage
