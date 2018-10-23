ae0592528cf50a631a86cc2ad923fcdb10685306
Kitteh <kitteh@namedkitten.pw>
2018-10-23 13:54:57 +0100

    Revert script changes.

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -4,11 +4,9 @@ set -x
 export OLDDIR=`pwd`
 export PATH="/usr/lib/ccache:/usr/lib/ccache/bin:$PATH"
 
-export CFLAGS="-fPIC -Os"
+export CFLAGS="-Os"
 
 ccache -C
-
-export V=0 VERBOSE=0
 
 #rm -rf mpv-build
 git clone --depth 1 https://github.com/mpv-player/mpv-build mpv-build
@@ -17,15 +15,12 @@ cd mpv-build
 export MPVDIR=`pwd`
 
 
-
-cd $MPVDIR
-
 #rm -rf ffmpeg mpv libass
 git clone --depth 1 https://github.com/FFmpeg/FFmpeg.git ffmpeg
 git clone --depth 1 https://github.com/mpv-player/mpv.git mpv
 git clone --depth 1 https://github.com/libass/libass.git libass
 #--enable-libaom
-echo '--disable-debug --disable-shared --disable-ffplay --disable-doc --enable-gpl --enable-version3 --enable-nonfree --enable-pthreads --enable-libvpx --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-avfilter --enable-filters' > ffmpeg_options
+echo ' ' > ffmpeg_options
 echo "--disable-programs --disable-runtime-cpudetect --enable-small" >> ffmpeg_options
 echo "--enable-libmpv-shared --prefix=/usr" > mpv_options
 echo "--disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-vapoursynth --disable-libsmbclient --disable-wayland" >> mpv_options
