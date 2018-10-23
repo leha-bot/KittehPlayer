9d695e2a3f34f3afd6092876c1581877a7ed6e46
Kitteh <kitteh@namedkitten.pw>
2018-10-23 23:23:41 +0100

    [Scripts] Fix upload script.

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -5,5 +5,5 @@ export TEXT=`git log -1 --pretty=%B`
 export UPLOADTOOL_BODY="$TEXT\nTravis CI build log: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID/"
 
 wget https://github.com/probonopd/uploadtool/raw/master/upload.sh
-bash upload.sh KittehPlayer*.AppImage* KittehPlayer*.exe
+bash upload.sh KittehPlayer*.AppImage*
 echo "Done!"
