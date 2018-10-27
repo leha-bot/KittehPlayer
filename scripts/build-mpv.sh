c53c1af30705ab87623aca671fe077f8fcfb4b60
Kitteh <kitteh@namedkitten.pw>
2018-10-27 19:46:35 +0100

    [Scripts] Vapoursynth.

--- scripts/build-mpv.sh
+++ scripts/build-mpv.sh
@@ -6,7 +6,7 @@ export PATH="/usr/lib/ccache:/usr/lib/ccache/bin:$PATH"
 
 export CFLAGS="-Os"
 
-rm -rf mpv-build
+#rm -rf mpv-build
 git clone --depth 1 https://github.com/mpv-player/mpv-build mpv-build
 cd mpv-build
 
@@ -18,7 +18,7 @@ rm -rf ffmpeg mpv libass
 echo ' ' > ffmpeg_options
 echo "--disable-programs --disable-runtime-cpudetect --enable-small" >> ffmpeg_options
 echo "--enable-libmpv-shared --prefix=/usr --enable-vapoursynth" > mpv_options
-echo "--disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-vapoursynth --disable-libsmbclient --disable-wayland" >> mpv_options
+echo "--disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib  --disable-tv --disable-debug-build --disable-manpage-build --disable-libsmbclient --disable-wayland" >> mpv_options
 
 ./rebuild -j`nproc`
 sudo ./install
